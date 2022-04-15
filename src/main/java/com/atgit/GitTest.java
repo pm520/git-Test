fde7260b2014711262528c23584fd8048fd4423d
pm520 <490323720@qq.com>
2022-04-15 20:01:59 +0800

    new hot-fix commit

--- src/main/java/com/atgit/GitTest.java
+++ src/main/java/com/atgit/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git! by hot-fix");
         System.out.println("hello git! by github");
         System.out.println("hello git! push");
+        System.out.println("hello git! by new hot-fix");
     }
 }
